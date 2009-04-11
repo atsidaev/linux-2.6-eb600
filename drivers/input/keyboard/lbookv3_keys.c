OpenInkpot-archive/linux-2.6

lbookv3_keys: Rewrite driver
mdelay(30) in keyboard IRQ handler was replaced by timer.
Long keypresses now is supported.

Signed-off-by: Yauhen Kharuzhy <jekhor@gmail.com>

drivers/input/keyboard/lbookv3_keys.c
@@ -28,30 +28,132 @@
 #include <asm/io.h>
 #include <asm/arch/regs-gpio.h>
 
-static int pullups[] = { 
+#define KEYB_DELAY		(15 * HZ / 1000)
+#define LONGPRESS_TIME		(HZ * 6 / 10) /* 0.6 seconds */
+
+static unsigned long int column_pins[] = {
 	S3C2410_GPG6, S3C2410_GPG7, S3C2410_GPG8, S3C2410_GPG9, S3C2410_GPG10,
 	S3C2410_GPG11, S3C2410_GPG4,
 };
 
-struct gpio_line {
-	unsigned char state;
-	unsigned char codes[7];
+static unsigned long int row_pins[] = {
+	S3C2410_GPF0, S3C2410_GPF1, S3C2410_GPF2,
 };
 
-static struct gpio_line lines[3] = {
-	[0] = {
-		.codes = { KEY_1, KEY_7, KEY_4, KEY_0, KEY_KPPLUS,
-			   KEY_ENTER, KEY_UP },
-	},
-	[1] = {
-		.codes = { KEY_6, KEY_3, KEY_9, KEY_RESERVED, KEY_KPMINUS,
-			   KEY_ESC, KEY_DOWN },
-	},
-	[2] = {
-		.codes = { KEY_2, KEY_8, KEY_5, KEY_RESERVED,
-			   KEY_RESERVED, KEY_RESERVED, KEY_RESERVED },
-	},
+static unsigned long keypad_state[ARRAY_SIZE(row_pins)][ARRAY_SIZE(column_pins)];
+
+
+static unsigned char keypad_codes[ARRAY_SIZE(row_pins)][ARRAY_SIZE(column_pins)] = {
+	{ KEY_1, KEY_7, KEY_4, KEY_0, KEY_KPPLUS, KEY_ENTER, KEY_UP },
+	{ KEY_6, KEY_3, KEY_9, KEY_RESERVED, KEY_KPMINUS, KEY_ESC, KEY_DOWN },
+	{ KEY_2, KEY_8, KEY_5, KEY_RESERVED, KEY_RESERVED, KEY_RESERVED, KEY_RESERVED }
 };
+
+static struct timer_list kb_timer;
+
+static inline void set_col(int col, int to)
+{
+	gpio_set_value(column_pins[col], to);
+}
+
+static void set_columns_to(int to)
+{
+	int j;
+
+	for (j = 0; j < ARRAY_SIZE(column_pins); j++) {
+		while ((!!gpio_get_value(column_pins[j]) != to))
+			gpio_set_value(column_pins[j], to);
+	}
+}
+
+static int wait_for_rows_high(void)
+{
+	int timeout, i;
+
+	for (timeout = 0xffff; timeout; timeout--) {
+		int is_low = 0;
+
+		for (i = 0; i < ARRAY_SIZE(row_pins); i++)
+			if (!gpio_get_value(row_pins[i]))
+				is_low = 1;
+		if (!is_low)
+			break;
+		udelay(10);
+	}
+	return timeout;
+}
+
+static void cfg_rows_to(unsigned int to)
+{
+	int i;
+
+	for (i = 0; i < ARRAY_SIZE(row_pins); i++)
+		s3c2410_gpio_cfgpin(row_pins[i], to);
+}
+
+
+static void generate_longpress_event(struct input_dev *input, unsigned char key)
+{
+	input_event(input, EV_KEY, KEY_LEFTALT, 1);
+	input_event(input, EV_KEY, key, 1);
+	input_event(input, EV_KEY, key, 0);
+	input_event(input, EV_KEY, KEY_LEFTALT, 0);
+	input_sync(input);
+}
+
+static void lbookv3_keys_kb_timer(unsigned long data)
+{
+	int row, col;
+	int pressed = 0;
+	struct input_dev *input = (struct input_dev *)data;
+
+	cfg_rows_to(S3C2410_GPIO_INPUT);
+	for (col = 0; col < ARRAY_SIZE(column_pins); col++) {
+		set_columns_to(1);
+		wait_for_rows_high();
+		set_col(col, 0);
+		udelay(30);
+
+		for (row = 0; row < ARRAY_SIZE(row_pins); row++) {
+			if (!gpio_get_value(row_pins[row])) {
+				if (!keypad_state[row][col]) {
+					keypad_state[row][col] = jiffies + LONGPRESS_TIME;
+				} else {
+					if (time_after(jiffies, keypad_state[row][col]) &&
+							(keypad_state[row][col] > 1)) {
+						generate_longpress_event(input, keypad_codes[row][col]);
+						keypad_state[row][col] = 1;
+					}
+				}
+				pressed = 1;
+			} else {
+				if (keypad_state[row][col]) {
+					unsigned char key = keypad_codes[row][col];
+
+					if (keypad_state[row][col] > 1) {
+						if (time_after(jiffies, keypad_state[row][col])) {
+							generate_longpress_event(input, key);
+						} else {
+							input_event(input, EV_KEY, key, 1);
+							input_event(input, EV_KEY, key, 0);
+							input_sync(input);
+						}
+					}
+
+					keypad_state[row][col] = 0;
+				}
+			}
+		}
+	}
+
+	set_columns_to(0);
+	udelay(10);
+	cfg_rows_to(S3C2410_GPIO_IRQ);
+	if (pressed & !timer_pending(&kb_timer)) {
+		kb_timer.expires = jiffies + KEYB_DELAY;
+		add_timer(&kb_timer);
+	}
+}
 
 static irqreturn_t lbookv3_powerkey_isr(int irq, void *dev_id)
 {
@@ -73,57 +175,11 @@ static irqreturn_t lbookv3_powerkey_isr(int irq, void *dev_id)
 
 static irqreturn_t lbookv3_keys_isr(int irq, void *dev_id)
 {
-	int i, j;
-	struct gpio_line *line;
-	struct input_dev *input = dev_id;
+	cfg_rows_to(S3C2410_GPIO_INPUT);
 
-	for (i = S3C2410_GPF0; i <= S3C2410_GPF2; i++)
-		s3c2410_gpio_cfgpin(i, S3C2410_GPIO_INPUT);
+	set_columns_to(1);
 
-	for (i = S3C2410_GPF0; i <= S3C2410_GPF2; i++) {
-		line = &lines[i - S3C2410_GPF0];
-		if (gpio_get_value(i)) {
-			if (!line->state)
-				continue;
-			for (j = 0; j < 7; j++)
-				if (line->state & (1 << j)) {
-					input_event(input, EV_KEY,
-						    line->codes[j], 0);
-					input_sync(input);
-				}
-			line->state = 0;
-			continue;
-		}
-
-
-		for (j = 0; j < ARRAY_SIZE(pullups); j++)
-			do {
-				gpio_set_value(pullups[j], 1);
-			} while (!gpio_get_value(pullups[j]));
-
-		mdelay(10);
-
-		for (j = 0; j < ARRAY_SIZE(pullups); j++) {
-			gpio_set_value(pullups[j], 0);
-			udelay(10);
-
-			if (!gpio_get_value(i)) {
-				line->state |= 1 << j;
-				input_event(input, EV_KEY, line->codes[j], 1);
-				input_sync(input);
-				break;
-			}
-		}
-
-		for (j = 0; j < ARRAY_SIZE(pullups); j++)
-			while (gpio_get_value(pullups[j]))
-				gpio_set_value(pullups[j], 0);
-
-		udelay(10);
-	}
-
-	for (i = S3C2410_GPF0; i <= S3C2410_GPF2; i++)
-		s3c2410_gpio_cfgpin(i, S3C2410_GPIO_SFN2);
+	lbookv3_keys_kb_timer((unsigned long)(dev_id));
 
 	return IRQ_HANDLED;
 }
@@ -132,11 +188,10 @@ static struct input_dev *input;
 static int __init lbookv3_keys_init(void)
 {
 	int i, j, error;
-	struct gpio_line *line;
 
-	for (i = 0; i < ARRAY_SIZE(pullups); i++) {
-		s3c2410_gpio_cfgpin(pullups[i], S3C2410_GPIO_OUTPUT);
-		gpio_set_value(pullups[i], 0);
+	for (i = 0; i < ARRAY_SIZE(column_pins); i++) {
+		s3c2410_gpio_cfgpin(column_pins[i], S3C2410_GPIO_OUTPUT);
+		gpio_set_value(column_pins[i], 0);
 	}
 
 	input = input_allocate_device();
@@ -153,13 +208,17 @@ static int __init lbookv3_keys_init(void)
 	input->id.product = 0x0001;
 	input->id.version = 0x0100;
 
-	for (i = S3C2410_GPF0; i <= S3C2410_GPF2; i++) {
-		line = &lines[i - S3C2410_GPF0];
+	setup_timer(&kb_timer, lbookv3_keys_kb_timer, (unsigned long)input);
+	memset(keypad_state, 0, sizeof(keypad_state));
 
-		for (j = 0; j < 7; j++)
-			if (line->codes[j] != KEY_RESERVED)
+	for (i = 0; i < ARRAY_SIZE(row_pins); i++) {
+		for (j = 0; j < 7; j++) {
+			if (keypad_codes[i][j] != KEY_RESERVED)
 				input_set_capability(input, EV_KEY,
-						line->codes[j]);
+						keypad_codes[i][j]);
+				input_set_capability(input, EV_KEY,
+						KEY_LEFTALT);
+		}
 	}
 
 	input_set_capability(input, EV_KEY, KEY_POWER);
@@ -231,6 +290,7 @@ static void __exit lbookv3_keys_exit(void)
 		free_irq(irq, input);
 	}
 
+	del_timer_sync(&kb_timer);
 	input_unregister_device(input);
 
 	input_free_device(input);

atsidaev/linux-2.6-eb600

drivers/input/keyboard/lbookv3_keys.c
@@ -28,30 +28,132 @@
 #include <mach/hardware.h>
 #include <mach/regs-gpio.h>
 
-static int pullups[] = { 
+#define KEYB_DELAY		(15 * HZ / 1000)
+#define LONGPRESS_TIME		(HZ * 6 / 10) /* 0.6 seconds */
+
+static unsigned long int column_pins[] = {
 	S3C2410_GPG6, S3C2410_GPG7, S3C2410_GPG8, S3C2410_GPG9, S3C2410_GPG10,
 	S3C2410_GPG11, S3C2410_GPG4,
 };
 
-struct gpio_line {
-	unsigned char state;
-	unsigned char codes[7];
+static unsigned long int row_pins[] = {
+	S3C2410_GPF0, S3C2410_GPF1, S3C2410_GPF2,
 };
 
-static struct gpio_line lines[3] = {
-	[0] = {
-		.codes = { KEY_1, KEY_7, KEY_4, KEY_0, KEY_KPPLUS,
-			   KEY_ENTER, KEY_UP },
-	},
-	[1] = {
-		.codes = { KEY_6, KEY_3, KEY_9, KEY_RESERVED, KEY_KPMINUS,
-			   KEY_ESC, KEY_DOWN },
-	},
-	[2] = {
-		.codes = { KEY_2, KEY_8, KEY_5, KEY_RESERVED,
-			   KEY_RESERVED, KEY_RESERVED, KEY_RESERVED },
-	},
+static unsigned long keypad_state[ARRAY_SIZE(row_pins)][ARRAY_SIZE(column_pins)];
+
+
+static unsigned char keypad_codes[ARRAY_SIZE(row_pins)][ARRAY_SIZE(column_pins)] = {
+	{ KEY_1, KEY_7, KEY_4, KEY_0, KEY_KPPLUS, KEY_ENTER, KEY_UP },
+	{ KEY_6, KEY_3, KEY_9, KEY_RESERVED, KEY_KPMINUS, KEY_ESC, KEY_DOWN },
+	{ KEY_2, KEY_8, KEY_5, KEY_RESERVED, KEY_RESERVED, KEY_RESERVED, KEY_RESERVED }
 };
+
+static struct timer_list kb_timer;
+
+static inline void set_col(int col, int to)
+{
+	gpio_set_value(column_pins[col], to);
+}
+
+static void set_columns_to(int to)
+{
+	int j;
+
+	for (j = 0; j < ARRAY_SIZE(column_pins); j++) {
+		while ((!!gpio_get_value(column_pins[j]) != to))
+			gpio_set_value(column_pins[j], to);
+	}
+}
+
+static int wait_for_rows_high(void)
+{
+	int timeout, i;
+
+	for (timeout = 0xffff; timeout; timeout--) {
+		int is_low = 0;
+
+		for (i = 0; i < ARRAY_SIZE(row_pins); i++)
+			if (!gpio_get_value(row_pins[i]))
+				is_low = 1;
+		if (!is_low)
+			break;
+		udelay(10);
+	}
+	return timeout;
+}
+
+static void cfg_rows_to(unsigned int to)
+{
+	int i;
+
+	for (i = 0; i < ARRAY_SIZE(row_pins); i++)
+		s3c2410_gpio_cfgpin(row_pins[i], to);
+}
+
+
+static void generate_longpress_event(struct input_dev *input, unsigned char key)
+{
+	input_event(input, EV_KEY, KEY_LEFTALT, 1);
+	input_event(input, EV_KEY, key, 1);
+	input_event(input, EV_KEY, key, 0);
+	input_event(input, EV_KEY, KEY_LEFTALT, 0);
+	input_sync(input);
+}
+
+static void lbookv3_keys_kb_timer(unsigned long data)
+{
+	int row, col;
+	int pressed = 0;
+	struct input_dev *input = (struct input_dev *)data;
+
+	cfg_rows_to(S3C2410_GPIO_INPUT);
+	for (col = 0; col < ARRAY_SIZE(column_pins); col++) {
+		set_columns_to(1);
+		wait_for_rows_high();
+		set_col(col, 0);
+		udelay(30);
+
+		for (row = 0; row < ARRAY_SIZE(row_pins); row++) {
+			if (!gpio_get_value(row_pins[row])) {
+				if (!keypad_state[row][col]) {
+					keypad_state[row][col] = jiffies + LONGPRESS_TIME;
+				} else {
+					if (time_after(jiffies, keypad_state[row][col]) &&
+							(keypad_state[row][col] > 1)) {
+						generate_longpress_event(input, keypad_codes[row][col]);
+						keypad_state[row][col] = 1;
+					}
+				}
+				pressed = 1;
+			} else {
+				if (keypad_state[row][col]) {
+					unsigned char key = keypad_codes[row][col];
+
+					if (keypad_state[row][col] > 1) {
+						if (time_after(jiffies, keypad_state[row][col])) {
+							generate_longpress_event(input, key);
+						} else {
+							input_event(input, EV_KEY, key, 1);
+							input_event(input, EV_KEY, key, 0);
+							input_sync(input);
+						}
+					}
+
+					keypad_state[row][col] = 0;
+				}
+			}
+		}
+	}
+
+	set_columns_to(0);
+	udelay(10);
+	cfg_rows_to(S3C2410_GPIO_IRQ);
+	if (pressed & !timer_pending(&kb_timer)) {
+		kb_timer.expires = jiffies + KEYB_DELAY;
+		add_timer(&kb_timer);
+	}
+}
 
 static irqreturn_t lbookv3_powerkey_isr(int irq, void *dev_id)
 {
@@ -73,57 +175,11 @@ static irqreturn_t lbookv3_powerkey_isr(int irq, void *dev_id)
 
 static irqreturn_t lbookv3_keys_isr(int irq, void *dev_id)
 {
-	int i, j;
-	struct gpio_line *line;
-	struct input_dev *input = dev_id;
+	cfg_rows_to(S3C2410_GPIO_INPUT);
 
-	for (i = S3C2410_GPF0; i <= S3C2410_GPF2; i++)
-		s3c2410_gpio_cfgpin(i, S3C2410_GPIO_INPUT);
+	set_columns_to(1);
 
-	for (i = S3C2410_GPF0; i <= S3C2410_GPF2; i++) {
-		line = &lines[i - S3C2410_GPF0];
-		if (s3c2410_gpio_getpin(i)) {
-			if (!line->state)
-				continue;
-			for (j = 0; j < 7; j++)
-				if (line->state & (1 << j)) {
-					input_event(input, EV_KEY,
-						    line->codes[j], 0);
-					input_sync(input);
-				}
-			line->state = 0;
-			continue;
-		}
-
-
-		for (j = 0; j < ARRAY_SIZE(pullups); j++)
-			do {
-				s3c2410_gpio_setpin(pullups[j], 1);
-			} while (!s3c2410_gpio_getpin(pullups[j]));
-
-		mdelay(10);
-
-		for (j = 0; j < ARRAY_SIZE(pullups); j++) {
-			s3c2410_gpio_setpin(pullups[j], 0);
-			udelay(10);
-
-			if (!s3c2410_gpio_getpin(i)) {
-				line->state |= 1 << j;
-				input_event(input, EV_KEY, line->codes[j], 1);
-				input_sync(input);
-				break;
-			}
-		}
-
-		for (j = 0; j < ARRAY_SIZE(pullups); j++)
-			while (s3c2410_gpio_getpin(pullups[j]))
-				s3c2410_gpio_setpin(pullups[j], 0);
-
-		udelay(10);
-	}
-
-	for (i = S3C2410_GPF0; i <= S3C2410_GPF2; i++)
-		s3c2410_gpio_cfgpin(i, S3C2410_GPIO_SFN2);
+	lbookv3_keys_kb_timer((unsigned long)(dev_id));
 
 	return IRQ_HANDLED;
 }
@@ -132,11 +188,10 @@ static struct input_dev *input;
 static int __init lbookv3_keys_init(void)
 {
 	int i, j, error;
-	struct gpio_line *line;
 
-	for (i = 0; i < ARRAY_SIZE(pullups); i++) {
-		s3c2410_gpio_cfgpin(pullups[i], S3C2410_GPIO_OUTPUT);
-		s3c2410_gpio_setpin(pullups[i], 0);
+	for (i = 0; i < ARRAY_SIZE(column_pins); i++) {
+		s3c2410_gpio_cfgpin(column_pins[i], S3C2410_GPIO_OUTPUT);
+		s3c2410_gpio_setpin(column_pins[i], 0);
 	}
 
 	input = input_allocate_device();
@@ -153,13 +208,17 @@ static int __init lbookv3_keys_init(void)
 	input->id.product = 0x0001;
 	input->id.version = 0x0100;
 
-	for (i = S3C2410_GPF0; i <= S3C2410_GPF2; i++) {
-		line = &lines[i - S3C2410_GPF0];
+	setup_timer(&kb_timer, lbookv3_keys_kb_timer, (unsigned long)input);
+	memset(keypad_state, 0, sizeof(keypad_state));
 
-		for (j = 0; j < 7; j++)
-			if (line->codes[j] != KEY_RESERVED)
+	for (i = 0; i < ARRAY_SIZE(row_pins); i++) {
+		for (j = 0; j < 7; j++) {
+			if (keypad_codes[i][j] != KEY_RESERVED)
 				input_set_capability(input, EV_KEY,
-						line->codes[j]);
+						keypad_codes[i][j]);
+				input_set_capability(input, EV_KEY,
+						KEY_LEFTALT);
+		}
 	}
 
 	input_set_capability(input, EV_KEY, KEY_POWER);
@@ -231,6 +290,7 @@ static void __exit lbookv3_keys_exit(void)
 		free_irq(irq, input);
 	}
 
+	del_timer_sync(&kb_timer);
 	input_unregister_device(input);
 
 	input_free_device(input);