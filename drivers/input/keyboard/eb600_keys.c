570a413c7da81c7909eab8f46a8407090fae637a
Alexandr Tsidaev <a.tsidaev@gmail.com>
2010-01-09 22:52:29 +0500

    EB600: keyboard and power button fix Signed-off-by: Alexandr Tsidaev <a.tsidaev@gmail.com>

--- drivers/input/keyboard/eb600_keys.c
+++ drivers/input/keyboard/eb600_keys.c
@@ -37,25 +37,33 @@
 static unsigned long poll_interval = KEYB_DELAY;
 static unsigned long longpress_time = LONGPRESS_TIME;
 
-static unsigned long int key_pins[] = { 
-	S3C2410_GPG2, S3C2410_GPG3, S3C2410_GPG0, S3C2410_GPG1, S3C2410_GPF7, 
-	S3C2410_GPF6, S3C2410_GPF5, S3C2410_GPF4, S3C2410_GPF3,
-	S3C2410_GPG4, S3C2410_GPG5,
-	S3C2410_GPF0 };
-	
-static unsigned long key_state[ARRAY_SIZE(key_pins)];	
-	
-static unsigned char key_codes[] = {
-	KEY_LEFT, KEY_RIGHT, KEY_UP, KEY_DOWN, KEY_ENTER,
-	KEY_DELETE, KEY_ESC, KEY_MEDIA, KEY_MENU,
-	KEY_VOLUMEUP, KEY_VOLUMEDOWN,
-	KEY_POWER };
+struct eb600_key_info
+{
+	unsigned long pin;
+	unsigned int pin_config;
+	unsigned char key_code;
+};
 
-static unsigned int key_irq[] = {
-	S3C2410_GPG2_EINT10, S3C2410_GPG3_EINT11, S3C2410_GPG0_EINT8, S3C2410_GPG1_EINT9, S3C2410_GPF7_EINT7,
-	S3C2410_GPF6_EINT6, S3C2410_GPF5_EINT5, S3C2410_GPF4_EINT4, S3C2410_GPF3_EINT3,
-	S3C2410_GPG4_EINT12, S3C2410_GPG5_EINT13,
-	S3C2410_GPF0_EINT0 };
+static struct eb600_key_info eb600_keys[] = {
+	{S3C2410_GPG2, S3C2410_GPG2_EINT10, KEY_LEFT},
+	{S3C2410_GPG3, S3C2410_GPG3_EINT11, KEY_RIGHT},
+	{S3C2410_GPG0, S3C2410_GPG0_EINT8, KEY_UP},
+	{S3C2410_GPG1, S3C2410_GPG1_EINT9, KEY_DOWN},
+	{S3C2410_GPF7, S3C2410_GPF7_EINT7, KEY_ENTER},
+	
+	{S3C2410_GPF6, S3C2410_GPF6_EINT6, KEY_DELETE},
+	{S3C2410_GPF5, S3C2410_GPF5_EINT5, KEY_ESC},
+	{S3C2410_GPF4, S3C2410_GPF4_EINT4, KEY_MEDIA},
+	{S3C2410_GPF3, S3C2410_GPF3_EINT3, KEY_MENU},
+	
+	{S3C2410_GPG4, S3C2410_GPG4_EINT12, KEY_VOLUMEUP},
+	{S3C2410_GPG5, S3C2410_GPG5_EINT13, KEY_VOLUMEDOWN},
+	
+	{S3C2410_GPF0, S3C2410_GPF0_EINT0, KEY_POWER},
+};
+
+	
+static unsigned long key_state[ARRAY_SIZE(eb600_keys)];	
 
 static struct timer_list kb_timer;	
 
@@ -74,21 +82,21 @@ static void eb600_keys_kb_timer(unsigned long data)
 	int pressed = 0;
 	struct input_dev *input = (struct input_dev *)data;
 
-	for (i = 0; i < ARRAY_SIZE(key_pins); i++) {
-		if (!s3c2410_gpio_getpin(key_pins[i])) {
+	for (i = 0; i < ARRAY_SIZE(eb600_keys); i++) {
+		if (!s3c2410_gpio_getpin(eb600_keys[i].pin)) {
 			if (!key_state[i]) {
 				key_state[i] = jiffies + longpress_time;
 			} else {
 				if (time_after(jiffies, key_state[i]) &&
 						(key_state[i] > 1)) {
-					generate_longpress_event(input, key_codes[i]);
+					generate_longpress_event(input, eb600_keys[i].key_code);
 					key_state[i] = 1;
 				}
 			}
 			pressed = 1;
 		} else {
 			if (key_state[i]) {
-				unsigned char key = key_codes[i];
+				unsigned char key = eb600_keys[i].key_code;
 					if (key_state[i] > 1) {
 					if (time_after(jiffies, key_state[i])) {
 						generate_longpress_event(input, key);
@@ -147,8 +155,8 @@ static struct input_dev *input;
 static int __init eb600_keys_init(void)
 {
 	int i, error;
-	for (i=0;i<ARRAY_SIZE(key_pins);i++)
-		s3c2410_gpio_cfgpin(key_pins[i], S3C2410_GPIO_INPUT);
+	for (i=0;i<ARRAY_SIZE(eb600_keys);i++)
+		s3c2410_gpio_cfgpin(eb600_keys[i].pin, S3C2410_GPIO_INPUT);
 		
 	input = input_allocate_device();
 	if (!input)
@@ -166,8 +174,8 @@ static int __init eb600_keys_init(void)
 
 	setup_timer(&kb_timer, eb600_keys_kb_timer, (unsigned long)input);
 	
-	for (i=0;i<ARRAY_SIZE(key_codes);i++)
-		input_set_capability(input, EV_KEY, key_codes[i]);
+	for (i=0;i<ARRAY_SIZE(eb600_keys);i++)
+		input_set_capability(input, EV_KEY, eb600_keys[i].key_code);
 		
 	error = input_register_device(input);
 	if (error) {
@@ -186,11 +194,11 @@ static int __init eb600_keys_init(void)
 
 	eb600_keys_isr(0, input);
 
-	for (i = 0; i < ARRAY_SIZE(key_pins); i++) {
-		int irq = key_irq[i];
+	for (i = 0; i < ARRAY_SIZE(eb600_keys); i++) {
+		int irq = s3c2410_gpio_getirq(eb600_keys[i].pin);
 
-		s3c2410_gpio_cfgpin(key_pins[i], S3C2410_GPIO_SFN2);
-		s3c2410_gpio_pullup(key_pins[i], 1);
+		s3c2410_gpio_cfgpin(eb600_keys[i].pin, eb600_keys[i].pin_config);
+		s3c2410_gpio_pullup(eb600_keys[i].pin, 1);
 
 		set_irq_type(irq, IRQ_TYPE_EDGE_BOTH);
 		error = request_irq(irq, eb600_keys_isr, IRQF_SAMPLE_RANDOM,
@@ -207,7 +215,7 @@ static int __init eb600_keys_init(void)
 	
 fail_reg_irqs:
 	for (i = i - 1; i >= 0; i--)
-		free_irq(key_irq[i], input);
+		free_irq(s3c2410_gpio_getirq(eb600_keys[i].pin), input);
 	device_remove_file(&input->dev, &dev_attr_poll_interval);
 	
 err_add_poll_interval:
@@ -221,8 +229,8 @@ static void __exit eb600_keys_exit(void)
 {
 	int i;
 
-	for (i = 0; i < ARRAY_SIZE(key_pins); i++) {
-		int irq = s3c2410_gpio_getirq(key_pins[i]);
+	for (i = 0; i < ARRAY_SIZE(eb600_keys); i++) {
+		int irq = s3c2410_gpio_getirq(eb600_keys[i].pin);
 
 		disable_irq_wake(irq);
 		free_irq(irq, input);
